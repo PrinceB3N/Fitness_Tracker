3352a74e2600518a368921dc1a962f71cf7062f0
PrinceB3N <benjaminprince@ucsb.edu>
2021-01-18 19:23:13 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Test(props){
+  return <h1>Hello, {props.name}</h1>;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edi  <code>src/App.js</code> and save to reload.
         </p>
+        <Test name="Benjamin" />
         <a
           className="App-link"
           href="https://reactjs.org"
